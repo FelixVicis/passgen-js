5f954fb39f78c571d37c74c91c61bfb86f17a3b3
Allen Mills <mills.allen.james@gmail.com>
2022-09-27 03:54:21 -0700

    update hash

--- hash.js
+++ hash.js
@@ -1,9 +1,17 @@
 const crypto = require('crypto');
 const blength = 5e5;
 
-module.exports = function rand_sha(){
+module.exports = makeRandomSha;
+
+function makeRandomSha(){
+	return makeShaFromSource(crypto.randomBytes(blength));
+};
+
+makeRandomSha.shaFromValue = makeShaFromSource;
+
+function makeShaFromSource(value) {
 	return crypto.createHash('sha256')
-		.update(crypto.randomBytes(blength))
+		.update(value)
 		.digest('hex')
 		.toString();
-};
+}
